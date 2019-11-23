9bc43c1c9fc7e987b84c5145c2d0d37336cc52c5
jasacvmurah <58102699+jasacvmurah@users.noreply.github.com>
2019-11-23 17:13:36 +0700

    Update data testimoni.js

--- data testimoni.js
+++ data testimoni.js
@@ -6,21 +6,27 @@ var dataOrder = {
         "template": "CV Desain CV004",
         "templateURL": "https://jasacvpurwokerto.blogspot.com/2019/11/desain-cv004.html",
     },
-    "Indri W": {
+    "0822-17..77..": {
         "screenshotURL": "https://www.instagram.com/p/B4ngmLCndAz/",
-        "avaURL": "https://1.bp.blogspot.com/-C0kenaAxjJ4/XdkAZ4AA0_I/AAAAAAAAA08/CzXcoR7zp-8Ovs6xtR917dF7zWnZIXh5gCLcBGAsYHQ/s1600/ava.png",
+        "avaURL": "https://1.bp.blogspot.com/-B94DgCP87XE/XdkEhgPgnfI/AAAAAAAAA1U/hjk6PjF2ewMvKm1A-C6ZpZmsSQfTCt6zgCLcBGAsYHQ/s1600/IMG-20191013-WA0002.jpg",
         "time": "2019-11-09",
         "template": "CV Desain CV013",
         "templateURL": "https://jasacvpurwokerto.blogspot.com/2019/11/desain-cv013.html",
     },
-    "Naufal": {
+    "082...79..70": {
         "screenshotURL": "https://www.instagram.com/p/B4ngkTwnLOi/",
-        "avaURL": "https://1.bp.blogspot.com/-C0kenaAxjJ4/XdkAZ4AA0_I/AAAAAAAAA08/CzXcoR7zp-8Ovs6xtR917dF7zWnZIXh5gCLcBGAsYHQ/s1600/ava.png",
+        "avaURL": "https://1.bp.blogspot.com/-eft2LkDNLA4/XdkFRRv2yhI/AAAAAAAAA1c/HZaJeSDivoIhYcu7OcArbtlYLZ-ByAlNgCLcBGAsYHQ/s1600/WhatsApp%2BImage%2B2019-11-07%2Bat%2B08.13.31.jpeg",
         "time": "2019-11-09",
         "template": "CV Desain CV004",
         "templateURL": "https://jasacvpurwokerto.blogspot.com/2019/11/desain-cv004.html",
     },
-    
+    "0812-98....55": {
+        "screenshotURL": "https://www.instagram.com/p/B4ngXIxH8df/",
+        "avaURL": "https://1.bp.blogspot.com/-Kb-zQb_Az4A/XdkGDOTPHvI/AAAAAAAAA1k/Li9_N-vb_JgTtc46IcVhemEr3cTVSASYACLcBGAsYHQ/s1600/WhatsApp%2BImage%2B2019-11-05%2Bat%2B16.14.31.jpeg",
+        "time": "2019-11-09",
+        "template": "CV Desain CV022",
+        "templateURL": "https://jasacvpurwokerto.blogspot.com/2019/11/desain-cv022.html",
+    },    
     
     
 };
